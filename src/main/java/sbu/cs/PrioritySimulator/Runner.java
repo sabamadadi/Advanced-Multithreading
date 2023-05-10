936fbc51e2532ed99cfc1eb656f1941452a9cf3b
Saba Madadi <sabamadadi9@gmail.com>
2023-05-10 22:15:38 +0330

    Update Runner.java

--- src/main/java/sbu/cs/PrioritySimulator/Runner.java
+++ src/main/java/sbu/cs/PrioritySimulator/Runner.java
@@ -1,63 +1,38 @@
-package sbu.cs.PrioritySimulator;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Runner {
-
     public static List<Message> messages = new ArrayList<>();
 
-    /**
-     * Simulate a priority system between Black, white, and blue threads.
-     * Given a number of each thread type, your program must first execute every Black thread, then
-     execute every Blue thread, and finally execute every white thread. (Black > Blue > White)
-     * Complete each thread type class by adding the specified code in the respective run() methods.
-
-     * The order of execution only matters when comparing two different thread types. This means two
-     black threads are not prioritised over each other (can swap their execution order) but between a
-     Black thread and a Blue thread, the Black thread must fully execute before the Blue thread starts.
-
-     * You are NOT ALLOWED to use the Thread.Sleep() method to solve this problem, as it does not
-     guarantee the desired thread execution order.
-     * You can utilize other classes and methods available in the java.util.concurrent package. It is
-     recommended that you use the "CountDownLatch" class to solve the problem.
-
-     * You are allowed to change the existing code in any of the given classes or add your own new code
-     to any of them.
-     * Your code must pass all of the test cases provided in the test folder.
-
-     * @param blackCount    number of black threads
-     * @param blueCount     number of blue threads
-     * @param whiteCount    number of white threads
-     */
     public void run(int blackCount, int blueCount, int whiteCount) throws InterruptedException {
         List<ColorThread> colorThreads = new ArrayList<>();
-
-        // TODO
+        CountDownLatch blackLatch = new CountDownLatch(blackCount);
+        CountDownLatch blueLatch = new CountDownLatch(blueCount);
 
         for (int i = 0; i < blackCount; i++) {
-            BlackThread blackThread = new BlackThread();
+            BlackThread blackThread = new BlackThread(blackLatch);
             colorThreads.add(blackThread);
             blackThread.start();
         }
 
-        // TODO
-
+        blackLatch.await();
         for (int i = 0; i < blueCount; i++) {
-            BlueThread blueThread = new BlueThread();
+            BlueThread blueThread = new BlueThread(blueLatch);
             colorThreads.add(blueThread);
             blueThread.start();
         }
 
-        // TODO
-
+        blueLatch.await(); 
         for (int i = 0; i < whiteCount; i++) {
             WhiteThread whiteThread = new WhiteThread();
             colorThreads.add(whiteThread);
             whiteThread.start();
         }
-
-        // TODO
+        
+        for (ColorThread thread : colorThreads) {
+            thread.join();
+        }
     }
 
     synchronized public static void addToList(Message message) {
@@ -68,7 +43,12 @@ public class Runner {
         return messages;
     }
 
-    public static void main(String[] args) {
-        // Use the main function to test the code yourself
+    public static void main(String[] args) throws InterruptedException {
+        Runner runner = new Runner();
+        runner.run(2, 3, 4);
+        List<Message> messages = runner.getMessages();
+        for (Message message : messages) {
+            System.out.println(message);
+        }
     }
 }
